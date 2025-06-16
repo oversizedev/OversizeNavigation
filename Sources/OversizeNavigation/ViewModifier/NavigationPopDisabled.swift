ce07b24a8e569f4a09bbf7afca92a3f36199d1c7
Alexander Romanov <aromanov07@gmail.com>
2025-06-16 22:34:54 +0300

    Fix macOS (#1)
    * Fix macOS
    
    * Fix CI

--- Sources/OversizeNavigation/ViewModifier/NavigationPopDisabled.swift
+++ Sources/OversizeNavigation/ViewModifier/NavigationPopDisabled.swift
@@ -5,31 +5,35 @@
 
 import SwiftUI
 
-extension UIView {
-    var parentViewController: UIViewController? {
-        sequence(first: self) {
-            $0.next
-        }.first { $0 is UIViewController } as? UIViewController
-    }
-}
-
-private struct NavigationPopGestureDisabler: UIViewRepresentable {
-    let disabled: Bool
-
-    func makeUIView(context _: Context) -> some UIView { UIView() }
-
-    func updateUIView(_ uiView: UIViewType, context _: Context) {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.02) {
-            uiView.parentViewController?.navigationController?.interactivePopGestureRecognizer?.isEnabled = !disabled
+#if os(iOS)
+    extension UIView {
+        var parentViewController: UIViewController? {
+            sequence(first: self) {
+                $0.next
+            }.first { $0 is UIViewController } as? UIViewController
         }
     }
-}
+
+    private struct NavigationPopGestureDisabler: UIViewRepresentable {
+        let disabled: Bool
+
+        func makeUIView(context _: Context) -> some UIView { UIView() }
+
+        func updateUIView(_ uiView: UIViewType, context _: Context) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.02) {
+                uiView.parentViewController?.navigationController?.interactivePopGestureRecognizer?.isEnabled = !disabled
+            }
+        }
+    }
+#endif
 
 public extension View {
     @ViewBuilder
     func interactivePopGestureDisabled(_ disabled: Bool) -> some View {
-        background {
-            NavigationPopGestureDisabler(disabled: disabled)
-        }
+        #if os(iOS)
+            background {
+                NavigationPopGestureDisabler(disabled: disabled)
+            }
+        #endif
     }
 }
